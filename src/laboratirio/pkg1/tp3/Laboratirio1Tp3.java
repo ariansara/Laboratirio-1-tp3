77d78edef5dacfe6715be49c3f58134b62f570ce
ariansara <102766969+ariansara@users.noreply.github.com>
2023-08-18 17:23:22 -0300

    Initial commit

---
+++ src/laboratirio/pkg1/tp3/Laboratirio1Tp3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratirio.pkg1.tp3;
+
+/**
+ *
+ * @author pollo
+ */
+public class Laboratirio1Tp3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
